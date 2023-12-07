91c2b6d2457bf95bc06e058cdbff1b0f45f17925
JSyro <jasyrotuck@gmail.com>
2023-12-06 22:48:22 -0800

    not quite

--- 1/sol.py
+++ 1/sol.py
@@ -3,7 +3,7 @@ import os
 folder_path = os.path.dirname(os.path.realpath(__file__))
 lines = open(f"{folder_path}/input.txt").readlines()
 
-print(f"# of lines:{len(lines)}")
+print(f"# of lines: {len(lines)}")
 
 def get_2_digit_num(line):
     first_digit = ''
@@ -23,26 +23,32 @@ result = 0
 for line in lines:
     result += get_2_digit_num(line)
 
-print(f"Part 1 result:{result}")
+print(f"Part 1 result: {result}")
 
 # PART 2
 
 num_to_int = {
-    "one": 1,
-    "two": 2,
-    "three": 3,
-    "four": 4,
-    "five": 5,
-    "six": 6,
-    "seven": 7,
-    "eight": 8,
     "nine": 9,
+    "eight": 8,
+    "seven": 7,
+    "six": 6,
+    "five": 5,
+    "four": 4,
+    "three": 3,
+    "two": 2,
+    "one": 1,
     "zero": 0,
 }
 result = 0
 
-for line in lines[:10]:    
+# this can't handle the substrings `twone` and `nineight`
+
+for line in lines:    
+    # print(line)
     for string,val in num_to_int.items():
         line = line.replace(string, str(val))
-    result += get_2_digit_num(line)
-print(f"Part 2 result:{result}")
+    # print(line)
+    number = get_2_digit_num(line)
+    # print(number)
+    result += number
+print(f"Part 2 result: {result}")
